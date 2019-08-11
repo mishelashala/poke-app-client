0d45b535ca3e86105432457acbf21778761c7654
Michell Ayala <starships@outlook.com>
2019-08-10 23:45:04 -0500

    refactor(PokemonDetailsService): remove wrapper promise from getDetailsByName

--- src/services/PokemonDetailsService.ts
+++ src/services/PokemonDetailsService.ts
@@ -9,15 +9,13 @@ export const PokemonDetailsService = (
   apiGateway: any
 ): IPokemonDetailsService => {
   // getDetailsByName :: String -> Promise IPokemonDetails
-  const getDetailsByName = (name: string): Promise<IPokemonDetails> => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const res = await apiGateway.getPokemonDetails(name);
-        resolve(pokemonDetailsMapper.toEntity(res.data));
-      } catch (err) {
-        reject(err);
-      }
-    });
+  const getDetailsByName = async (name: string): Promise<IPokemonDetails> => {
+    try {
+      const res = await apiGateway.getPokemonDetails(name);
+      return Promise.resolve(pokemonDetailsMapper.toEntity(res.data));
+    } catch (err) {
+      return Promise.reject(err);
+    }
   };
 
   return {
