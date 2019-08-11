ac611ee1046956c0c356912a20b1932dbfb80834
Michell Ayala <starships@outlook.com>
2019-08-10 23:21:22 -0500

    refactor(App): from class to SFC

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,18 @@ import { HomeView } from "./views/Home";
 import { PokemonDetailsView } from "./views/PokemonDetails";
 import { configureStore } from "./configureStore";
 
-interface IPokemon {
-  name: string;
-  url: string;
-}
-
-interface IAppState {
-  pokemons: IPokemon[];
-}
-
-class App extends React.Component<{}, IAppState> {
-  render() {
-    return (
-      <Provider store={configureStore()}>
-        <Router>
-          <React.Fragment>
-            <NavBar />
-            <Switch>
-              <Route path="/" exact component={HomeView} />
-              <Route
-                path="/pokemon/:pokemonName"
-                component={PokemonDetailsView}
-              />
-            </Switch>
-          </React.Fragment>
-        </Router>
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <Provider store={configureStore()}>
+    <Router>
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={HomeView} />
+          <Route path="/pokemon/:pokemonName" component={PokemonDetailsView} />
+        </Switch>
+      </React.Fragment>
+    </Router>
+  </Provider>
+);
 
 export default App;
